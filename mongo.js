b760ad26e43d3d927d022db64695d7eb135534bc
Paweł Gesek <pgesek@soldevelo.com>
2017-04-10 18:30:41 +0200

    #9: Fixed routes for ILR in HIM
    Go to localhost:5001 in order to get to ILR

--- mongo.js
+++ mongo.js
@@ -1,25 +1,102 @@
 var client={
-    'clientID': 'openinfoman',
-    'name': 'OpenInfoMan',
-    'passwordAlgorithm': 'sha512',
-    "passwordSalt": '72a00618ac4493ce12d833d4cf9a975f',
-    'passwordHash':'f1c79ab2cf8b5c7c9ab0c701433ae1499d7511d206f3b272913d0d1762c1ebf177bc7540cf58e885f8e2955359f913c4ffa6b8772f7eeb7aa4dbc634b0dd2349',
-    'roles': ['irl'],
-    '__v': 0
-}, channel={
-    'name': 'Interlinked Registry',
-    'urlPattern': '/CSD||/static',
-    'allow': ['openinfoman'],
-    'routes': {
-        'name': 'IRL route',
-        'primary': true,
-        'host': '$ILR_HOST',
-        'port': 8984,
-        'secured': false,
-        'username': 'openinfoman',
-        'password': 'openinfoman'
-    }
+      'clientID': 'openinfoman',
+      'name': 'OpenInfoMan',
+      'passwordAlgorithm': 'sha512',
+      "passwordSalt": '72a00618ac4493ce12d833d4cf9a975f',
+      'passwordHash':'f1c79ab2cf8b5c7c9ab0c701433ae1499d7511d206f3b272913d0d1762c1ebf177bc7540cf58e885f8e2955359f913c4ffa6b8772f7eeb7aa4dbc634b0dd2349',
+      'roles': ['irl'],
+      '__v': 0
+}, irl_channel={
+      "name": "Interlinked Registry",
+      "urlPattern": "^/CSD",
+      "allow": [
+        "irl"
+      ],
+      "routes": [
+        {
+          "name": "IRL route",
+          "primary": true,
+          "host": "ohie-ilr",
+          "port": 8984,
+          "secured": false,
+          "path": "",
+          "forwardAuthHeader": true,
+          "status": "enabled",
+          "type": "http"
+        }
+      ],
+      "tcpPort": null,
+      "tcpHost": null,
+      "pollingSchedule": null,
+      "matchContentJson": null,
+      "matchContentValue": null,
+      "matchContentXpath": null,
+      "matchContentRegex": null,
+      "responseBody": true,
+      "requestBody": true,
+      "type": "http",
+      "whitelist": [],
+      "authType": "private",
+      "matchContentTypes": [],
+      "properties": [],
+      "txViewAcl": [],
+      "txViewFullAcl": [],
+      "txRerunAcl": [],
+      "alerts": [],
+      "status": "enabled",
+      "rewriteUrls": true,
+      "addAutoRewriteRules": true,
+      "rewriteUrlsConfig": [],
+      "autoRetryEnabled": false,
+      "autoRetryPeriodMinutes": 60
+}, irl_static_channel={
+      "requestBody": true,
+      "responseBody": true,
+      "urlPattern": "^/static",
+      "name": "Interlinked Registry /static",
+      "matchContentRegex": null,
+      "matchContentXpath": null,
+      "matchContentValue": null,
+      "matchContentJson": null,
+      "pollingSchedule": null,
+      "tcpHost": null,
+      "tcpPort": null,
+      "autoRetryPeriodMinutes": 60,
+      "autoRetryEnabled": false,
+      "rewriteUrlsConfig": [],
+      "addAutoRewriteRules": true,
+      "rewriteUrls": false,
+      "status": "enabled",
+      "alerts": [],
+      "txRerunAcl": [],
+      "txViewFullAcl": [],
+      "txViewAcl": [],
+      "properties": [],
+      "matchContentTypes": [],
+      "routes": [
+        {
+          "password": "",
+          "username": "",
+          "primary": true,
+          "pathTransform": "",
+          "path": "",
+          "port": 8984,
+          "host": "ohie-ilr",
+          "secured": false,
+          "name": "IRL Static Route",
+          "forwardAuthHeader": false,
+          "status": "enabled",
+          "type": "http"
+        }
+      ],
+      "authType": "private",
+      "whitelist": [],
+      "allow": [
+        "irl"
+      ],
+      "type": "http"
 };
 
 db.clients.insert(client);
-db.channels.insert(channel);
+db.channels.insert(irl_channel);
+db.channels.insert(irl_static_channel);
